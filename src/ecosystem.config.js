373572c43a67e1a90b84f4f66aea8befc32027b0
DarioMarret <javier_dario_marret@hotmail.com>
2022-09-30 15:08:50 -0500

    up

--- src/ecosystem.config.js
+++ src/ecosystem.config.js
@@ -8,10 +8,11 @@ module.exports = {
     instances: 1,
     env: {
       NODE_ENV: "production",
-      PORT: "5055",
+      PORT: "5056",
       HOST: "0.0.0.0",
       BASIC_USERNAME: "",
       BASIC_PASSWORD: "",
+      TOKEN:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IlRlY25vUm9ib3QiLCJyb2xlIjoiQm90IiwibmJmIjoxNjQ0NTM0MDY0LCJleHAiOjE2NzYwOTE1OTksImlhdCI6MTY0NDUzNDA2NCwiaXNzIjoid3d3Lnh0cmltdHZjYWJsZS5jb20uZWMiLCJhdWQiOiJ3d3cueHRyaW10dmNhYmxlLmNvbS5lYyJ9.XgPwH8PKlsUMPRUravJlUrJEJT81onOHdr4QU8SKnHU"
     },
     env_development: {
       NODE_ENV: "development"

0870f482f4a850d5a3a15d0c898c9b21473c3f7b
DarioMarret <javier_dario_marret@hotmail.com>
2022-10-06 14:08:41 -0500

    up

--- src/controller/turnos.js
+++ src/controller/turnos.js
@@ -34,24 +34,9 @@ export const listarTurnos = async (req, reply) => {
 
 export const createTurno = async (req, reply) => {
 
-    const { user_id, agencia_id, horario, cedula, nombres, referencias, correo, fecha_turno, tipo_turno } = req.body
-    // const { data, status } = await axios.post(`https://turnostvc.intelnexo.com/api/CreateTurnoCliente`, {
-    //     user_id,
-    //     agencia_id,
-    //     horario,
-    //     cedula,
-    //     nombres,
-    //     referencias,
-    //     correo,
-    //     fecha_turno,
-    //     tipo_turno
-    // })
-    fetch(`https://turnostvc.intelnexo.com/api/CreateTurnoCliente`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
+    try {
+        const { user_id, agencia_id, horario, cedula, nombres, referencias, correo, fecha_turno, tipo_turno } = req.body
+        const { data, status } = await axios.post(`https://turnostvc.intelnexo.com/api/CreateTurnoCliente`, {
             user_id,
             agencia_id,
             horario,
@@ -62,21 +47,23 @@ export const createTurno = async (req, reply) => {
             fecha_turno,
             tipo_turno
         })
-    }).then(res => res.json())
-        .then(json => {
-            console.log(json)
+        if (status == 200) {
+
             reply.code(200).send({
                 success: true,
-                dara: json
+                dara: data
             })
+
+        }
+    } catch (error) {
+
+    } finally {
+        reply.code(200).send({
+            success: true,
+            dara: data
         })
-        .catch(err => {
-            console.log(err)
-            reply.code(200).send({
-                success: false,
-                msg: err
-            })
-        })
+    }
+
 }
 
 

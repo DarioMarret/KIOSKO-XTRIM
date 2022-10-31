297c753513293fdd6c574a9715780aba935d1e44
DarioMarret <64284053+DarioMarret@users.noreply.github.com>
2022-10-31 09:32:36 -0500

    Update EnviatTokenEmail.js

--- src/function/EnviatTokenEmail.js
+++ src/function/EnviatTokenEmail.js
@@ -5,7 +5,7 @@ import axios from "axios"
 export const EnviarTokenEmail = async (email, token) => {
 
     try {
-        const { data, status } = await axios.post('https://automation.xfiv.chat/sender_email/email/notificaciones', {
+        const { data, status } = await axios.post('https://auto.xfiv.chat/webhook-test/Xtrim-emails', {
             email,
             asunto: "Token de verificación Kiosko",
             content: `<h1>Bienvenido a Kiosko Xtrim</h1>
